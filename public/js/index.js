e6716bf8e65d0f1e1aa19ab26e63ae7a0342427c
mateus <mateusweslley14@gmail.com>
2025-11-06 21:59:32 -0300

    Add initial implementation of Nikel application with login, registration, and transaction management features
    - Created CSS styles for the application layout and components.
    - Developed HTML structure for the home, index, and transactions pages.
    - Implemented JavaScript functionality for user login and account creation, including form validation and local storage management.
    - Added modal dialogs for user registration and transaction entry.
    - Integrated Bootstrap for responsive design and UI components.

---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+const myModal = new bootstrap.Modal("#register-modal");
+
+// LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email-input').value;
+    const password = document.getElementById('password-input').value;
+
+    console.log(email, password)
+})
+
+// CRIAR CONTA
+document.getElementById("create-form").addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email-create-input').value;
+    const password = document.getElementById('password-create-input').value;
+
+    if (email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return
+    }
+
+
+    if (password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 caracteres.");
+        return
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+
+    myModal.hide()
+    alert("Conta criada com sucesso!");
+}
+)
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
